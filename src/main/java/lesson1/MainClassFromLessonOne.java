d98a7761a2008ec57c794d2838f16595f0e2faa2
NechaevMichael <nechaevmichael@mail.ru>
2023-03-26 13:50:03 +0500

    init repo

---
+++ src/main/java/lesson1/MainClassFromLessonOne.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class MainClassFromLessonOne {
+    public static void main(String[] args) {
+        exeOne();
+    }
+
+    private static void exeOne() {
+        System.out.println("научите меня программировать.");
+    }
+}
