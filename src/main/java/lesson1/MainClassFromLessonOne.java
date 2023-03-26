375b397d28bc44f5aa2f1c90192f2704b9cedee4
NechaevMichael <nechaevmichael@mail.ru>
2023-03-26 14:15:59 +0500

    add task1

--- src/main/java/lesson1/MainClassFromLessonOne.java
+++ src/main/java/lesson1/MainClassFromLessonOne.java
@@ -1,11 +1,20 @@
 package lesson1;
-
+import java.util.Scanner;
 public class MainClassFromLessonOne {
     public static void main(String[] args) {
         exeOne();
     }
 
     private static void exeOne() {
-        System.out.println("Научите меня программировать.");
+        // Задание №1. Написать программу, которая запросит пользователя ввести имя в консоли.
+        // Получит введённую строку и выведет в консоль сообщение "Привет, Имя!".
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите своё имя: ");
+        String name = scanner.nextLine();
+        System.out.printf("Привет, %s!", name);
+        scanner.close();
     }
+
+
+
 }
