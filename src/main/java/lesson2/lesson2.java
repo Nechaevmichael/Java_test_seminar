abf3459bf4115f321af3e1982a9331beb56e9a48
NechaevMichael <nechaevmichael@mail.ru>
2023-03-28 00:26:31 +0500

    add countchar

--- src/main/java/lesson2/lesson2.java
+++ src/main/java/lesson2/lesson2.java
@@ -1,9 +1,14 @@
 package lesson2;
 import java.util.Scanner;
+import java.lang.String;
 public class lesson2 {
     public static void main(String[] args) {
-        createString();
+//        createString();
+        countchar();
+//        palindrome();
     }
+//    Дано четное число N (>0) и символы c1 и c2.
+//    Написать метод, который вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1.
     private static void createString() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите длину строки: ");
@@ -19,5 +24,33 @@ public class lesson2 {
             }
         }
         System.out.println(sb.toString());
+    }
+//    Напишите метод, который сжимает строку.
+//    Пример: вход aaaabbbcdd.
+    private static void countchar() {
+        String str = "aaaabbbcdddd";
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                if (count == 1) {
+                    result.append(str.charAt(i));
+                } else {
+                    result.append(str.charAt(i)).append(count);
+                    count = 1;
+                }
+            }
+        }
+        System.out.println(result.append(str.charAt(str.length() - 1)).append(count > 1 ? count: " ").toString());
+    }
+
+    //    Напишите метод, который принимает на вход строку (String) и определяет
+//    является ли строка палиндромом (возвращает boolean значение).
+    private static void palindrome() {
+        String str = "asdf";
+
     }
 }
